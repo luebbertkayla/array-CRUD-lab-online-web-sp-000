b442a10aab8694444147ee26a4193685c01979b9
Kayla Luebbert <luebbertkayla@gmail.com>
2021-01-31 22:53:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   colors = ["purple", "black", "green", "blue"]
-  colors.unshift("orange")
+  colors.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
