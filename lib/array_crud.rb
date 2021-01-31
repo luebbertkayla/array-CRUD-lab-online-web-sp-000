f4de0818cf60735721dbea75ec5f713f1e8b5e46
Kayla Luebbert <luebbertkayla@gmail.com>
2021-01-31 22:57:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  colors = ["purple", "black", "green", "blue"]
+  colors = ["purple", "black", "green", "arrays!"]
   colors.pop()
 end
 
