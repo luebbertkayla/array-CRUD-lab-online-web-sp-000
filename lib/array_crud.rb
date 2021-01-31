ac1409d9d9c8638b32506daa969b983ef27bdcd9
Kayla Luebbert <luebbertkayla@gmail.com>
2021-01-31 23:00:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  colors = ["wow", "black", "green", "blue"]
+  return colors.first
 end
 
 def retrieve_last_element_from_array(array)
